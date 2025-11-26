dab2daf6f17ead80886cfe7d39d0fb7066cf414f
Artem Kamysh <kmr4619a@ensfsi.onmicrosoft.com>
2025-11-26 10:16:41 +0100

    fin de form

---
+++ src/model/FormulaireSpectacle.java
@@ -0,0 +1,21 @@
+package model;
+
+public class FormulaireSpectacle  extends Formulaire {
+	private int numZone;
+	private int numChaise;
+	
+	public FormulaireSpectacle(int jour, int mois, int numZone, int numChaise) {
+		super(jour, mois);
+		this.numZone = numZone;
+		this.numChaise = numChaise;
+	}
+	
+	public int getNumZone() {
+		return numZone;
+	}
+	
+	public int getnumChaise() {
+		return numChaise;
+	}
+	
+}
